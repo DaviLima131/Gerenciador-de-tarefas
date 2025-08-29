363a126e2329739c170e350cf941cdb0ea82e09a
DAVI LIMA OLIVEIRA <davi.1902@aluno.pr.senac.br>
2025-08-29 09:12:18 -0300

    Criando tela de login, criando botao de logout com ícone

--- login.php
+++ login.php
@@ -19,7 +19,7 @@
             <div class="col-md-4">
                 <div class="card card-body mt-5">
                     <h3 class="text-center">Login</h3>
-                    <form action="#" method="POST">
+                    <form action="authenticate.php" method="POST">
                         <div class="form-group">
                             <input type="email" name="email" id="email" class="form-control"
                             placeholder="exemplo@gmail.com" required autofocus>
@@ -31,7 +31,7 @@
                         <button type="submit" class="btn btn-success btn-block">Entrar</button>
                     </form>
                     <div class="text-center mt3">
-                        <a href="#" class="text-secondary">Cadastrar</a>
+                        <a href="register.php" class="text-secondary">Cadastrar</a>
                     </div>
                 </div>
             </div>
